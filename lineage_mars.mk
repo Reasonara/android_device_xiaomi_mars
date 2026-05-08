f4e68111030a101811858f1e5c376ea657a1ffd1
Reasonara <156067960+Reasonara@users.noreply.github.com>
2026-05-09 00:12:33 +0900

    Update lineage_mars.mk

--- lineage_mars.mk
+++ lineage_mars.mk
@@ -33,6 +33,3 @@ AXION_MAINTAINER := Dipsan
 # Processor name (underscores become spaces)
 AXION_PROCESSOR := Snapdragon_888
 
-TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-TARGET_ENABLE_BLUR := true
